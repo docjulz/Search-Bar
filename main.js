471aee49ae760602c64f7f904c721551b7aa40ff
Julian Miller <73328279+docjulz@users.noreply.github.com>
2022-04-05 20:37:52 -0700

    Init Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInfo = document.querySelector("[data-search]")
+
+// loop through json by creating an object
+let users = []
+
+// Update search bar to show results
+searchInfo.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    users.forEach(user => {
+        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value)
+        user.element.classList.toggle("hide", !isVisible)
+    })
+   
+})
+
+// pull data from API and add it to JS file
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+        users = data.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0]
+            const header = card.querySelector("[data-header]")
+            const body = card.querySelector("[data-body]")
+            header.textContent = user.name
+            body.textContent = user.email
+            userCardContainer.append(card)   
+            return {name: user.name, email: user.email, element: card}  
+        })
+    })
+
+
---
+++ notes.md
@@ -0,0 +1,2 @@
+# Name Search with JSON
+this project pulls a list of names and contact info from an external JSON resource and allows user to filter by name once loaded
